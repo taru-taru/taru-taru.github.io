a1249bcdfbbb53825e98634f4de4ca4707ba1517
taru-taru <80304411+taru-taru@users.noreply.github.com>
2021-03-09 14:09:42 +0900

    oknir

---
+++ index.js
@@ -0,0 +1,25 @@
+function mySort() {
+    // (1) ノードリストを取得
+    var myUL = document.getElementById("test");
+    var myNodeList = myUL.querySelectorAll("li");
+    // (2) 配列を得る
+    var myArray = Array.prototype.slice.call(myNodeList);
+    // (3) 配列をソート
+    function compareText (a,b) {
+        if (a.innerHTML < b.innerHTML)
+            return 1;
+        else if (a.innerHTML > b.innerHTML)
+            return -1;
+        return 0;
+        }
+    myArray.sort(compareText);
+    // (4) 新しい順番を DOM ツリーに反映
+    for (var i=0; i<myArray.length; i++) {
+        myUL.appendChild(myUL.removeChild(myArray[i]))
+    }
+ console.log(myArray[0])
+}
+
+window.onload = function() {
+ mySort();
+};
---
+++ page.js
@@ -0,0 +1,124 @@
+var inputElem = document.getElementById('page'); // input要素
+var currentValueElem = document.getElementById('bar'); // 埋め込む先のspan要素
+
+  var img = document.getElementById('ill');
+
+function OnButtonClick(pp) {
+  var pNumber= ( '000' + Number(pp) ).slice( -3 );
+  img.setAttribute('src', pNumber+".jpg");
+  img.setAttribute('class', pNumber);
+  page.value=pNumber;
+  currentValueElem.innerText = Number(pp);
+}
+
+// 現在の値をspanに埋め込む関数
+function setCurrentValue(val){
+  var chang= ( '000' + Number(val) ).slice( -3 );
+  currentValueElem.innerText = val;
+  var change= chang+".jpg";
+  img.setAttribute('src', change);
+  img.setAttribute('class', chang);
+}
+// inputイベント時に値をセットする関数
+function rangeOnChange(e){
+  setCurrentValue(e.target.value);
+}
+window.onload = function(){
+  inputElem.addEventListener('input', rangeOnChange); // スライダー変化時にイベントを発火
+  setCurrentValue(inputElem.value); // ページ読み込み時に値をセット
+}
+
+
+var chang,change;
+function setSwipe(elem) {
+    var t = document.querySelector(elem);
+    var startX;        // タッチ開始 x座標
+    var startY;        // タッチ開始 y座標
+    var moveX;    // スワイプ中の x座標
+    var moveY;    // スワイプ中の y座標
+    var dist = 30;    // スワイプを感知する最低距離（ピクセル単位）
+     
+    // タッチ開始時： xy座標を取得
+    t.addEventListener("touchstart", function(e) {
+        e.preventDefault();
+        startX = e.touches[0].pageX;
+        startY = e.touches[0].pageY;
+    });
+     
+    // スワイプ中： xy座標を取得
+    t.addEventListener("touchmove", function(e) {
+        e.preventDefault();
+        moveX = e.changedTouches[0].pageX;
+        moveY = e.changedTouches[0].pageY;
+    });
+     
+    // タッチ終了時： スワイプした距離から左右どちらにスワイプしたかを判定する/距離が短い場合何もしない
+    t.addEventListener("touchend", function(e) {
+        var img = document.getElementById('ill');
+        var src = img.getAttribute('src');
+        var num = Number(img.className);
+        if (startX > moveX && startX > moveX + dist) {        // unからupにスワイプ
+            if(num<Number(inputElem.getAttribute('max'))){
+              var num= num+1;
+            }
+        }else if (startX < moveX && startX + dist < moveX) {    // upからunにスワイプ
+            if(num>1){
+              var num=num-1;
+            }
+        };
+        var chang= ( '000' + num ).slice( -3 );
+        var change= chang+".jpg";
+        img.setAttribute('src', change);
+        img.setAttribute('class', chang);
+        page.value=num;
+        currentValueElem.innerText = num;
+    });
+};
+
+
+
+
+ window.addEventListener("load", function(){
+   setSwipe('#ill');
+ });
+
+
+$(function(){
+ var btn = $(".tag ul li.parent");
+ var submenu = $(".child");
+ var submenulink = $(".child ul li a");
+  //click
+	$(btn).bind("click", "focus", function(event){
+     var shownav = $(this).find(".child");
+    if($(shownav).css("display")=="none"){
+		$(shownav).slideDown("fast");
+    }else{
+		$(shownav).slideUp("fast");
+   }
+ });
+
+});
+
+
+window.addEventListener('keydown',key);
+
+function key(event) {
+        var img = document.getElementById('ill');
+        var src = img.getAttribute('src');
+        var num = Number(img.className);
+        if (event.keyCode==39) {        // unからupにスワイプ
+            if(num<Number(inputElem.getAttribute('max'))){
+              var num= num+1;
+            }
+        }else if (event.keyCode==37) {    // upからunにスワイプ
+            if(num>1){
+              var num=num-1;
+            }
+        };
+        var chang= ( '000' + num ).slice( -3 );
+        var change= chang+".jpg";
+        img.setAttribute('src', change);
+        img.setAttribute('class', chang);
+        page.value=num;
+        currentValueElem.innerText = num;
+};
