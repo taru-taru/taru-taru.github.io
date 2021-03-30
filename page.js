75756e215e841a7ff49db2d192f8abaee13e3e73
taru-taru <80304411+taru-taru@users.noreply.github.com>
2021-03-30 14:45:02 +0900

    a

--- page.js
+++ page.js
@@ -24,7 +24,10 @@ window.onload = function(){
   inputElem.addEventListener('input', rangeOnChange); // スライダー変化時にイベントを発火
   OnButtonClick(inputElem.value); // ページ読み込み時に値をセット
   setSwipe('#ill');
-  startWindow()
+  startWindow();
+  if (quary1.length>3) {
+   let BB=document.querySelector("b");
+   BB.style.display = 'nune';}
 };
 
 
